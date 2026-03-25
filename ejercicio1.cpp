441b882630ebc7bcd21eb2e0a0b75019677b5fd1
Santiago Duró <142905316+SantiiDuro@users.noreply.github.com>
2026-03-25 12:33:51 -0300

    modifico gitignore

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -7,5 +7,14 @@ using namespace std;
 
 int main()
 {
+    int cantSalas;
+    string cadena;
+
+    cin >> cantSalas;
+    cin >> cadena;
+
+    cout << "Cant salas: " << cantSalas << endl;
+    cout << "Cadena: " << cadena << endl;
+
     return 0;
 }
