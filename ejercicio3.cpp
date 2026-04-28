729a84b0bb8d254840fd8b46176db59aa6b90789
Santiago Duró <142905316+SantiiDuro@users.noreply.github.com>
2026-04-28 16:54:12 -0300

    Ejercicio 3 terminado, con todos los tests OK

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -2,11 +2,55 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "tads/BinaryHeap.h"
+#include "tads/BinaryHeapImp.cpp"
 
 using namespace std;
 
+struct Patient
+{
+    int id;
+    int arrivalTime;
+    int urgencyLevel;
+    int insertionOrder;
+};
+
+bool patientComparator(Patient p1, Patient p2)
+{
+    if (p1.urgencyLevel != p2.urgencyLevel)
+    {
+        return p1.urgencyLevel > p2.urgencyLevel;
+    }
+    if (p1.arrivalTime != p2.arrivalTime)
+    {
+        return p1.arrivalTime < p2.arrivalTime;
+    }
+
+    return p1.insertionOrder < p2.insertionOrder;
+}
+
 int main()
 {
-    // TODO
+    int patientsCount = 0;
+
+    cin >> patientsCount;
+
+    HeapImp<Patient> emergencyQueue(patientsCount, patientComparator);
+
+    for (int i = 0; i < patientsCount; i++)
+    {
+        Patient p;
+        cin >> p.id >> p.arrivalTime >> p.urgencyLevel;
+        p.insertionOrder = i;
+        emergencyQueue.insert(p);
+    }
+
+    while (!emergencyQueue.isEmpty())
+    {
+        Patient next = emergencyQueue.top();
+        emergencyQueue.removeTop();
+        cout << next.id << endl;
+    }
+
     return 0;
 }
--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile
-g++ -o ej1 ejercicio1.cpp -std=c++11
+g++ -o ej3 ejercicio3.cpp -std=c++11
 
 if [ $? -ne 0 ]; then
     echo "Compilation failed"
@@ -9,7 +9,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Test directory
-TEST_DIR="tests/ejercicio1"
+TEST_DIR="tests/ejercicio3"
 
 # Counters
 PASSED=0
@@ -22,7 +22,7 @@ for input in $TEST_DIR/*.in.txt; do
     expected="$TEST_DIR/$name.out.txt"
     
     # Run test
-    ./ej1 < "$input" > temp.out.txt
+    ./ej3 < "$input" > temp.out.txt
     
     # Compare
     if diff -q temp.out.txt "$expected" > /dev/null; then
@@ -35,7 +35,7 @@ for input in $TEST_DIR/*.in.txt; do
 done
 
 # Cleanup
-rm -f temp.out.txt ej1
+rm -f temp.out.txt ej3
 
 # Summary
 echo ""
